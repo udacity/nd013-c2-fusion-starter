95639f0146c95b8571dd66b590202793496833ab
Mattia Haas <mattia.haas@traila.ch>
2023-12-27 16:46:02 +0100

    Set settings in loop over dataset for ex 1

--- loop_over_dataset.py
+++ loop_over_dataset.py
@@ -53,7 +53,7 @@ import misc.params as params
 data_filename = "training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord"  # Sequence 1
 # data_filename = 'training_segment-10072231702153043603_5725_000_5745_000_with_camera_labels.tfrecord' # Sequence 2
 # data_filename = 'training_segment-10963653239323173269_1924_000_1944_000_with_camera_labels.tfrecord' # Sequence 3
-show_only_frames = [0, 200]  # show only frames in interval for debugging
+show_only_frames = [0, 1]  # show only frames in interval for debugging
 
 ## Prepare Waymo Open Dataset file for loading
 data_fullpath = os.path.join(
@@ -64,11 +64,11 @@ datafile = WaymoDataFileReader(data_fullpath)
 datafile_iter = iter(datafile)  # initialize dataset iterator
 
 ## Initialize object detection
-configs_det = det.load_configs(model_name="fpn_resnet")  # options are 'darknet', 'fpn_resnet'
+configs_det = det.load_configs(model_name="darknet")  # options are 'darknet', 'fpn_resnet'
 model_det = det.create_model(configs_det)
 
 configs_det.use_labels_as_objects = (
-    False  # True = use groundtruth labels as objects, False = use model-based detection
+    False  # True = use ground-truth labels as objects, False = use model-based detection
 )
 
 ## Uncomment this setting to restrict the y-range in the final project
@@ -83,16 +83,9 @@ camera = None  # init camera sensor object
 np.random.seed(10)  # make random values predictable
 
 ## Selective execution and visualization
-exec_detection = [
-    "bev_from_pcl",
-    "detect_objects",
-    "validate_object_labels",
-    "measure_detection_performance",
-]  # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
-exec_tracking = []  # options are 'perform_tracking'
-exec_visualization = (
-    []
-)  # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
+exec_detection = []  # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
+exec_tracking = ['perform_tracking']  # options are 'perform_tracking'
+exec_visualization = ["show_range_image"] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
 exec_list = make_exec_list(exec_detection, exec_tracking, exec_visualization)
 vis_pause_time = 0  # set pause time between frames in ms (0 = stop between frames until key is pressed)
 
