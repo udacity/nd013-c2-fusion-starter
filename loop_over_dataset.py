f3bfe036cc3dce9d1eea8c4bd3ecd189c0ba1f71
Mattia Haas <mattia.haas@traila.ch>
2023-12-29 16:15:38 +0100

    Make small changes in loop to run code for exercise

--- loop_over_dataset.py
+++ loop_over_dataset.py
@@ -53,7 +53,7 @@ import misc.params as params
 data_filename = "training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord"  # Sequence 1
 # data_filename = 'training_segment-10072231702153043603_5725_000_5745_000_with_camera_labels.tfrecord' # Sequence 2
 # data_filename = 'training_segment-10963653239323173269_1924_000_1944_000_with_camera_labels.tfrecord' # Sequence 3
-show_only_frames = [0, 1]  # show only frames in interval for debugging
+show_only_frames = [80, 100]  # show only frames in interval for debugging
 
 ## Prepare Waymo Open Dataset file for loading
 data_fullpath = os.path.join(
@@ -84,8 +84,8 @@ np.random.seed(10)  # make random values predictable
 
 ## Selective execution and visualization
 exec_detection = []  # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
-exec_tracking = ['perform_tracking']  # options are 'perform_tracking'
-exec_visualization = ["show_range_image"] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
+exec_tracking = ["perform_tracking"]  # options are 'perform_tracking'
+exec_visualization = ["show_pcl"]  # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
 exec_list = make_exec_list(exec_detection, exec_tracking, exec_visualization)
 vis_pause_time = 0  # set pause time between frames in ms (0 = stop between frames until key is pressed)
 
