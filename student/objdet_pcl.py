37c2d070787af974e991884501c0bf2121bcb9b6
Mattia Haas <mattia.haas@traila.ch>
2023-12-29 15:28:18 +0100

    Implement pcl visualization ex 2

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -32,6 +32,7 @@ import misc.objdet_tools as tools
 
 # Helper module for data decompression
 import zlib
+import open3d as o3d
 
 
 # visualize lidar point-cloud
@@ -40,15 +41,26 @@ def show_pcl(pcl):
     #######
     print("student task ID_S1_EX2")
 
+    def right_arrow_pressed(vis):
+        print("Moving to next frame")
+        vis.destroy_window()
+
     # step 1 : initialize open3d with key callback and create window
+    vis = o3d.visualization.VisualizerWithKeyCallback()
+    vis.create_window()
 
     # step 2 : create instance of open3d point-cloud class
+    pcd = o3d.geometry.PointCloud()
 
     # step 3 : set points in pcd instance by converting the point-cloud into 3d vectors (using open3d function Vector3dVector)
+    pcd.points = o3d.utility.Vector3dVector(pcl[:, :3])
 
-    # step 4 : for the first frame, add the pcd instance to visualization using add_geometry; for all other frames, use update_geometry instead
+    # step 4 : for the first frame, add the pcd instance to visualization using add_geometry
+    vis.add_geometry(pcd)
 
     # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)
+    vis.register_key_callback(262, right_arrow_pressed)
+    vis.run()
 
     #######
     ####### ID_S1_EX2 END #######
